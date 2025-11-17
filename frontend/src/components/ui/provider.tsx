b4c36800129751b4b43d470f1d67c369fb74ea6f
Nicolas Campbell <nicolas.campbell@gmail.com>
2025-11-16 16:07:54 -0800

    Updated light/dark mode to default to system theme. Added front end linter workflow and pull request workflow that lints and runs all tests.

--- frontend/src/components/ui/provider.tsx
+++ frontend/src/components/ui/provider.tsx
@@ -9,7 +9,7 @@ import { Toaster } from "./toaster"
 export function CustomProvider(props: PropsWithChildren) {
   return (
     <ChakraProvider value={system}>
-      <ColorModeProvider defaultTheme="light">
+      <ColorModeProvider defaultTheme="system">
         {props.children}
       </ColorModeProvider>
       <Toaster />
